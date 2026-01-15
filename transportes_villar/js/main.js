f34c82dde53bb8ac24d28992a7e0a545b729eb21
ConradoFrancisco <102098237+ConradoFrancisco@users.noreply.github.com>
2026-01-14 21:13:28 -0300

    villar landing

---
+++ transportes_villar/js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', function () {
+    const nav = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+// Implementation of smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
